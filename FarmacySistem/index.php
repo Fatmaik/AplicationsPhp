8a74e5e9e3354412e42eb9c5e75d02a7f84f8add
Dionathan <dionathan_bass@hotmail.com>
2017-03-30 17:43:43 -0300

    pag init done

---
+++ FarmacySistem/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>FarmacySistem</title>
+    <link rel="stylesheet" href="css/EstiloIndex/estilo.css">
+</head>
+<body>
+    <header>ESCOLHA A OPÇÂO DE ACESSO:</header>
+
+    <article>
+        <a href=""><img src="css/EstiloIndex/farmacia.png" alt="Site"></a>
+        <a href=""><img src="css/EstiloIndex/loja.png" alt="loja"></a>
+
+        
+        
+    </article>
+</body>
+</html>
