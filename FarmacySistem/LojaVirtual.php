28af8f1829930362191e4b9d695c75a15bab0ad0
Dionathan <dionathan_bass@hotmail.com>
2017-03-31 17:06:16 -0300

    Section Done/Article init

--- FarmacySistem/LojaVirtual.php
+++ FarmacySistem/LojaVirtual.php
@@ -10,18 +10,49 @@
     <script src="js/JsLV/javascript.js"></script>
 </head>
 <body>
-    <header>
+    <header class="cadPed">
         <div class="pedidos">Meus Pedidos</div>
         <div class="cadastro">Meu Cadastro</div>
     </header>
+    
     <section>
-        <img class="logo" src="css/EstiloLV/loja.png" alt="logo">
-        <div class="campoBusca">
-            <form action="$_GET" method="LV.php">    
-                <input class="procura" type="text" placeholder="O que você procura?">    
-                <input class="submit" type="image" src="css/EstiloLV/lupa.png" value="submit">              
-            </form>
-        </div>
+        <img class="logo" src="css/EstiloLV/img/loja.png" alt="logo">
+        
+        <form action="$_GET" method="LV.php" class="campoBusca">    
+            <input class="procura" type="text" placeholder="O que você procura?">    
+            <input class="submit" type="image" src="css/EstiloLV/img/lupa.png" value="submit">              
+        </form>       
+        
+        <ul>   
+            <li class="imgMed"><img src="css/EstiloLV/img/medicamentos.png" alt=""width="35px"></li>
+            <li id="medic">Medicamentos</li>   
+            <li class="imgSau"><img src="css/EstiloLV/img/saude.png" alt="" width="53px"></li>
+            <li id="saude">Saude</li>
+            <li class="imgBel"><img src="css/EstiloLV/img/escova.png" alt="" width="37px"></li>
+            <li id="beleza">Beleza</li>
+        </ul>  
     </section>
+    <article class="article">
+        .
+            <div id="boxArticle">
+                <img src="css/EstiloLV/img/tituloMedic.png" alt="">
+                <hr>
+                <p>Alergia</p>
+                <p>Diabete</p>
+                <p>Emagrecimento</p>
+                <p>Inflamação</p>
+                <p>Anestésico</p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                
+
+
+            </div>
+            <div id="hr"></div>
+        </div>
+    </article>
 </body>
 </html>
