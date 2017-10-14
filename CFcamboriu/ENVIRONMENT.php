b066e79245aa8b5b0e955e72f5fa3e6d6d87129b
Dionathan <dionathan_bass@hotmail.com>
2017-10-13 21:11:15 -0300

    teste de projeto

--- CFcamboriu/ENVIRONMENT.php
+++
@@ -1,6 +0,0 @@
-<?php
-// global setando em qual ambiente estou trabalhando
-### 1 - DEVELOPMENT_HOME = em casa
-### 2 - DEVELOPMENT_WORK = no trabalho
-### 3 - PRODUCTION = ambiente em producao
-define("ENVIRONMENT", "DEVELOPMENT_HOME");
